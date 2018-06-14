929e24e78c6dfbd2d7ecd943d92931ffa6b83005
heze <895315401@qq.com>
2018-06-14 20:49:22 +0800

    Initial Commit

---
+++ IDCardRecognition/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IDCardRecognition
+//
+//  Created by 何泽的mac on 2018/6/14.
+//  Copyright © 2018年 何泽的mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
